cf8ad06cfe9e2c3459cfe155d4dd91b01ca2ae24
Gianni <gianni@sistema3.it>
2017-04-30 16:49:14 +0200

    Update copyright

--- pjonboot.cpp
+++ pjonboot.cpp
@@ -1,25 +1,28 @@
 /*
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	This project is an attempt to create an ATmega bootloader over PJON bus.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+	Note that in this early phase, my main concern is about hex size,
+	because it's limited to ~4000 bytes. Also, this will run over SWBB.
+	___________________________________________________________________________
 
-___________________________________________________________________________
+	Thanks to Giovanni Blu Mitolo <gioscarab@gmail.com> for the amazing PJON project
+	___________________________________________________________________________
 
-Thanks to Giovanni Blu Mitolo <gioscarab@gmail.com> for the amazing PJON project
-___________________________________________________________________________
+	Copyright 2017 @johnkiller
 
-This project is an attempt to create an ATmega bootloader over PJON bus.
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-Note that in this early phase, my main concern is about hex size,
-because it's limited to ~4000 bytes. Also, this will run over SWBB.
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
 */
 
 //Currently tested only on Arduino Nano
